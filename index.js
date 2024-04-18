14715008bd87de2ca0bf6e94060867c39d1810e5
MeshariAlsh <165266292+MeshariAlsh@users.noreply.github.com>
2024-04-18 20:56:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,25 +12,23 @@ app.get('/tshirt', (req, res) => {
 
     const modelAI = spawn('python', ['Website_test.py'])
 
-    modelAI.on('close', (code) => {
-        console.log(`python exited code ${code}`)
-
-    } )
-
-    const imagePath = path.join(__dirname, 'test.jpg');  // Path to your image file
-    fs.readFile(imagePath, { encoding: 'base64' }, (err, data) => {
-        if (err) {
-            res.status(500).json({ error: 'Failed to load image' });
-            return;
-        }
-        res.json({ image: `data:image/jpeg;base64,${data}` });
-     });
- });
-
-// error handling 
-modelAI.stderr.on('data', (data) => {
+    modelAI.stderr.on('data', (data) => {
         console.error(`stderr: ${data}`);
     });
+
+    modelAI.on('close', (code) => {
+        console.log(`python exited code ${code}`);
+        
+        const imagePath = path.join(__dirname, 'test.jpg');  // Path to your image file
+        fs.readFile(imagePath, { encoding: 'base64' }, (err, data) => {
+            if (err) {
+            res.status(500).json({ error: 'Failed to load image' });
+            return;
+            }
+            res.json({ image: `data:image/jpeg;base64,${data}` });
+        });
+    });
+ });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
